5e4ce8fd8bf3e3ab973186f7d96eedc7a09a52c6
hunhwa01 <seungbin018838@gmail.com>
2025-01-09 16:41:11 +0900

    Delete static/main_home_pli.js

--- static/main_home_pli.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const playButton = document.querySelector('.music-player button');
-    playButton.addEventListener('click', () => {
-        alert('재생 버튼 클릭!');
-    });
-});
